7a1c8bf76b7489dfad34dd8bd8b6ab454f4b82e0
yangchao0033 <757097678@qq.com>
2015-01-03 02:18:54 +0800

    第二个playground 用来学习Swift字符串

---
+++ 0102/04-第二个play.playground/section-1.swift
@@ -0,0 +1,24 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var a = 10
+++a
+var btn = UIButton()
+btn.frame = CGRectMake(100, 100, 100, 100)
+btn.backgroundColor = UIColor.redColor()
+//btn.layer.cornerRadius = 12
+var s = "Hello" + "World"
+var ss = "hehe"
+var sss = s + ss
+sss = s
+
+let hand = 2
+var age = 20
+var s1 = "我今年\(age) " + String(hand) + " 我有\(hand)只手"
+var s2 = String(hand)
+var s3 = "\(hand)"
+var 你好 = "nihao"
+let 😜 = 45
