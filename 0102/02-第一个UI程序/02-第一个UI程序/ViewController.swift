13a447a22f29fa0cb7a2ca972f5cf3b4bdd1186a
yangchao0033 <757097678@qq.com>
2015-01-03 01:01:24 +0800

    Initial Commit

---
+++ 0102/02-第一个UI程序/02-第一个UI程序/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  02-第一个UI程序
+//
+//  Created by 杨超 on 15-1-3.
+//  Copyright (c) 2015年 yangchao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
