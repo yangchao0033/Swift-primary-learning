fc52affd6638269850b682485eab4036888c3fab
yangchao0033 <757097678@qq.com>
2015-01-03 00:37:26 +0800

    到底行不行？

--- 0102/02HelloWorld/02HelloWorld/main.swift
+++ 0102/02HelloWorld/02HelloWorld/main.swift
@@ -1,6 +1,6 @@
 //
 //  main.swift
-//  01-HelloWorld
+//  02HelloWorld
 //
 //  Created by 杨超 on 15-1-3.
 //  Copyright (c) 2015年 yangchao. All rights reserved.
---
+++ 01HelloWorld/01HelloWorld/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  01HelloWorld
+//
+//  Created by 杨超 on 15-1-3.
+//  Copyright (c) 2015年 yangchao. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
