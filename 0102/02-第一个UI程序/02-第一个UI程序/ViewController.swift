8faec2f14a1f59e40991d30a7d0245d120d9e511
yangchao0033 <757097678@qq.com>
2015-01-03 01:27:41 +0800

    第一个UI程序

--- 0102/02-第一个UI程序/02-第一个UI程序/ViewController.swift
+++ 0102/02-第一个UI程序/02-第一个UI程序/ViewController.swift
@@ -13,6 +13,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var btn = UIButton()
+        btn.frame = CGRectMake(100, 100, 100, 100)
+        btn.backgroundColor = UIColor.redColor()
+        self.view.addSubview(btn)
+        
+        var imageView = UIImageView()
+        imageView.image = UIImage(named: "002")
+        imageView.frame = CGRectMake(100, 250, 50, 50)
+        self.view.addSubview(imageView)
     }
 
     override func didReceiveMemoryWarning() {
