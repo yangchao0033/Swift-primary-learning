49d7906cf1153c8248889b8604f67aa630da4672
yangchao0033 <757097678@qq.com>
2015-01-03 01:40:06 +0800

    第一个playground

---
+++ 0102/03-第一个play.playground/section-1.swift
@@ -0,0 +1,12 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+//var str = "Hello, playground"
+
+var btn = UIButton()
+btn.frame = CGRectMake(100, 100, 100, 100)
+btn.backgroundColor = UIColor.redColor()
+btn.layer.cornerRadius = 12
+
+btn
