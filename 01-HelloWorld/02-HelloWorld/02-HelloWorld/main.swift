070bc48c2a165b0501ec6ba05e40dcd8296d8aa1
yangchao0033 <757097678@qq.com>
2015-01-03 00:15:09 +0800

    文件夹下好像可以使用嵌套git

---
+++ 01-HelloWorld/02-HelloWorld/02-HelloWorld/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  02-HelloWorld
+//
+//  Created by 杨超 on 15-1-3.
+//  Copyright (c) 2015年 yangchao. All rights reserved.
+//
+
+//import Foundation
+
+println("Hello, World!")
+println("Hello, World!")
+println("Hello, World!")
+println("Hello, World!")
+println("Hello, World!")
+println("Hello, World!5656")
+
